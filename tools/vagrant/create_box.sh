3c3da798f66c10b151681ec5eb0772813d7af2bb
Aleksey Mykhailov <aleksey.mykhailov@tubemogul.com>
2018-12-22 09:45:19 -0800

    add chaos challange, reorganise files

--- tools/vagrant/create_box.sh
+++ tools/vagrant/create_box.sh
@@ -3,8 +3,8 @@ set -e
 rm -f laptop.box
 vagrant destroy -f
 vagrant up
-vagrant package --output laptop.box --vagrantfile Vagrantfile.laptop
+vagrant package --output laptop.box
 vagrant destroy -f
-vagrant box list | grep laptop  && vagrant box remove laptop
+vagrant box list | grep laptop  && vagrant box remove -f laptop
 vagrant box add laptop.box --name laptop
 
--- tools/vagrant/provision_scripts/create_local_registry.sh
+++ tools/vagrant/provision_scripts/create_local_registry.sh
@@ -3,7 +3,9 @@ set -e
 
 IMAGES="nginx:latest
 oleksiimy/server:0.1
-oleksiimy/client:0.1"
+oleksiimy/client:0.1
+ayushsobti/kube-monkey:v0.3.0
+saykumar/ts2019:s1"
 
 kubectl rollout status deployment/registry --namespace=container-registry
 for image in $IMAGES; do
