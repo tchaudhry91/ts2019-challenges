ceb7358ed4da7011ac7960cf8a6f952f71d2d846
Aleksey Mykhailov <aleksey.mykhailov@tubemogul.com>
2019-01-31 16:09:34 -0800

    update vagrant box and and ops tools doc

--- tools/vagrant/provision_scripts/docker-composer.sh
+++
@@ -1,3 +0,0 @@
-sudo curl -L https://github.com/docker/compose/releases/download/1.23.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-echo "alias docker-compose='sudo docker-compose -H unix://var/snap/microk8s/current/docker.sock'" >> ~/.bash_aliases
