6d7b837b50c5c496262314e1f93c7bec494ec152
Aleksey Mykhailov <aleksey.mykhailov@tubemogul.com>
2019-02-02 10:54:58 -0800

    update orchestration challenge

---
+++ challanges/devops/orchestration/setup.sh
@@ -0,0 +1,13 @@
+kubectl delete -f k8s/smaster.yaml
+kubectl delete -f k8s/sminion.yaml
+
+
+kubectl create -f k8s/smaster.yaml
+kubectl create -f k8s/sminion.yaml
+
+kubectl rollout status StatefulSet/sminion && kubectl rollout status StatefulSet/smaster
+kubectl get --no-headers=true pods -l name=sminion -o custom-columns=:metadata.name | xargs -I {} kubectl exec {} -- sh -c  'echo "master: smaster-0.smaster.default.svc.cluster.local">/etc/salt/minion'
+kubectl get --no-headers=true pods -l name=sminion -o custom-columns=:metadata.name | xargs -I {} kubectl exec {} /etc/init.d/salt-minion restart
+
+kubectl exec smaster-0 -- salt-key -L
+kubectl exec -it smaster-0 -- salt-key -A
