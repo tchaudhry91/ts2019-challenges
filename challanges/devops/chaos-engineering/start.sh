a8b9fe0dfd7003e80e43ed603dd814a1f04ab9b3
Aleksey Mykhailov <aleksey.mykhailov@tubemogul.com>
2019-02-04 09:43:03 -0800

    update cleanup for chaos-engineering

--- challanges/devops/chaos-engineering/start.sh
+++ challanges/devops/chaos-engineering/start.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-kubectl delete  -f km-deploy.yaml 2>/dev/null
-kubectl -n smart-a-app delete  po,svc --all 2>/dev/null
-kubectl delete  -f namespace.yaml 2>/dev/null
+kubectl delete -f km-deploy.yaml 2>/dev/null
+kubectl delete -f namespace.yaml 2>/dev/null
 
 git reset --hard
 git clean -f -d
