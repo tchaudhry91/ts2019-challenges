cd691992f9ae448c71f47b1405f31f5cb31dc4ae
Aleksey Mykhailov <aleksey.mykhailov@tubemogul.com>
2019-02-04 17:18:35 -0800

    fix salt

--- challanges/devops/orchestration/setup.sh
+++ challanges/devops/orchestration/setup.sh
@@ -12,7 +12,7 @@ kubectl create -f k8s/sminion.yaml
 
 kubectl rollout status StatefulSet/sminion && kubectl rollout status StatefulSet/smaster
 kubectl get --no-headers=true pods -l name=sminion -o custom-columns=:metadata.name | xargs -I {} kubectl exec {} -- sh -c  'echo "master: smaster-0.smaster.default.svc.cluster.local">/etc/salt/minion'
-kubectl get --no-headers=true pods -l name=sminion -o custom-columns=:metadata.name | xargs -I {} kubectl exec {} /etc/init.d/salt-minion restart
+kubectl get --no-headers=true pods -l name=sminion -o custom-columns=:metadata.name | xargs -I {} kubectl exec {} -- sh -c '/etc/init.d/salt-minion restart && sleep 5 && /etc/init.d/salt-minion status'
 
 kubectl exec smaster-0 -- salt-key -L
 kubectl exec -it smaster-0 -- salt-key -A -y
